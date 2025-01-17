01d43adf6d70279090452450d4a4a37dccec53c4
Edgard <pateteedgard@gmail.com>
2025-01-17 16:26:44 -0400

    Update index.php
    Ordene el index

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 $op = 0;
-
 include "./Paginas/enunciado.php";
-
 include "./Html/Head.php";
 include "./Html/Header.php";
 include "./Html/Inicio_sesion.php";
 include "./Html/Footer.php";
 
-
+//inicio de la aplicación 
