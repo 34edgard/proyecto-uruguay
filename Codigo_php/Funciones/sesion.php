b061165fa8f73f94fa98e2613dfb7d21bb195af3
Edgard Patete <pateteedgard@gmail.com>
2025-02-24 20:05:48 -0400

    cambios del gitignore

--- Codigo_php/Funciones/sesion.php
+++ Codigo_php/Funciones/sesion.php
@@ -4,10 +4,10 @@
   $iniciar_sesion = function () {
     $extras = func_get_args();
     extract($_POST);
-    // var_dump($extras);
+  
     session_start();
     $arreglo = $extras[1][0]($cedula, $contraseña);
-    //return;
+
     if ($arreglo[0]) {
       $_SESSION["ci"] = $arreglo[1][0]['cedula'];
       $_SESSION["contraseña"] = $arreglo[1][0]['contrasena'];
